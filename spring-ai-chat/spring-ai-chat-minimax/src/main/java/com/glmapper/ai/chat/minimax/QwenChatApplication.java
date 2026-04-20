07d850ab99a7643a5bfc7030ecb9086ba9f0f2af
zhangyi <zhangyi@dcrays.cn>
2026-04-20 18:21:02 +0800

    1

---
+++ spring-ai-chat/spring-ai-chat-minimax/src/main/java/com/glmapper/ai/chat/minimax/QwenChatApplication.java
@@ -0,0 +1,13 @@
+package com.glmapper.ai.chat.minimax;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QwenChatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QwenChatApplication.class, args);
+    }
+
+}
